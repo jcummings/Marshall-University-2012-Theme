e4d50ddd1de3b0d1abd6870883aa351d28afc2f1
Eden Parker <boline@marshall.edu>
2012-05-14 10:08:48 -0400

    Added marco theme to bootstrap

---
+++ marco/assets/js/application.js
@@ -0,0 +1,6 @@
+<!-- This script corrects an issue with Opera Mini's tendency to over-simplify javascript and allows the nav-bar button to be clickable -->    
+  // $(document).ready(function() {
+  // if(operamini) {
+  // $('.btn-navbar').click(function() {});
+  // }
+  // });
