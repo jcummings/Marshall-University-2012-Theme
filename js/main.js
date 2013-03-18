c986c581f994598e44b7bf209377b8df03b327ce
Eden Parker <boline@marshall.edu>
2013-03-18 10:26:58 -0400

    Downgrades the version of js to support the msie call and removes the debugging script

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-// JavaScript Document
-if(!console){
-	console = { log: function(text){ alert(text);} }
-}
-
-
 /*
  * jQuery hashchange event - v1.3 - 7/21/2010
  * http://benalman.com/projects/jquery-hashchange-plugin/
@@ -46,4 +40,16 @@ $().ready(
 		$(window).hashchange(shiftWindow);
 	}
 );
+
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-2931131-1']);
+  _gaq.push(['_setDomainName', 'marshall.edu']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
 
